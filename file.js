ed82ee599a44baa374b862beef5aadf73f0a84c6
joaopaulovasconcelos <joao.paulo@3cplusnow.com>
2024-10-23 13:03:33 +0000

    test

---
+++ file.js
@@ -0,0 +1,10 @@
+let fireflights = 1000000
+let beliveInyourEyes = true;
+
+if(fireflights == 1000000){
+    beliveInyourEyes = false
+}
+
+if (fireflights != 1000000){
+    beliveInyourEyes = true;
+}
